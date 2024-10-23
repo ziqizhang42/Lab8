5e12114fe4c7f191e7b4c09c4f5ed66c491b8a31
Ziqi Zhang <ziqizhang42@gmail.com>
2024-10-23 17:32:15 -0600

    New project

---
+++ app/src/main/java/com/example/lab8v2/City.java
@@ -0,0 +1,21 @@
+package com.example.lab8v2;
+
+public class City {
+
+    private String city;
+    private String province;
+
+    public City(String city, String province){
+        this.city = city;
+        this.province = province;
+    }
+
+    String getCityName(){
+        return this.city;
+    }
+
+    String getProvinceName(){
+        return this.province;
+    }
+}
+
